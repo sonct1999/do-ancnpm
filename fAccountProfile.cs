f8244b148d75f4d662cf86d244a61654c2c77f2f
sonct1999 <76861293+sonct1999@users.noreply.github.com>
2021-01-26 04:40:47 +0700

    first commit

---
+++ fAccountProfile.cs
@@ -0,0 +1,102 @@
+﻿using QuanLyNhaHang.DAO;
+using QuanLyNhaHang.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaHang
+{
+    public partial class fAccountProfile : Form
+    {
+        private Account loginAccount;
+
+        public Account LoginAccount
+        {
+            get { return loginAccount; }
+            set { loginAccount = value; ChangeAccount(loginAccount); }
+        }
+        public fAccountProfile(Account acc)
+        {
+            InitializeComponent();
+
+            LoginAccount = acc;
+        }
+
+        void ChangeAccount(Account acc)
+        {
+            txtTendangnhap.Text = LoginAccount.UserName;
+            txtDisplayName.Text = LoginAccount.DisplayName;
+        }
+
+        void UpdateAccountInfo()
+        {
+            string displayName = txtDisplayName.Text;
+            string password = txtPassword.Text;
+            string newpass = txtNewPassword.Text;
+            string reenterPass = txtRewrite.Text;
+            string userName = txtTendangnhap.Text;
+
+            if (!newpass.Equals(reenterPass))
+            {
+                MessageBox.Show("Mật khẩu không trùng khớp!!!");
+            }
+            else
+            {
+                if (AccountDAO.Instance.UpdateAccount(userName, displayName, password, newpass))
+                {
+                    MessageBox.Show("Cập nhật thành công!");
+                    if (updateAccount != null)
+                        updateAccount(this, new AccountEvent(AccountDAO.Instance.GetAccountByUserName(userName)));
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu không trùng khớp!!!");
+                }
+            }
+        }
+
+        private event EventHandler<AccountEvent> updateAccount;
+        public event EventHandler<AccountEvent> UpdateAccount
+        {
+            add { updateAccount += value; }
+            remove { updateAccount -= value; }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateAccountInfo();
+        }
+
+        private void txtTendangnhap_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class AccountEvent : EventArgs
+    {
+        private Account acc;
+
+        public Account Acc
+        {
+            get { return acc; }
+            set { acc = value; }
+        }
+
+        public AccountEvent(Account acc)
+        {
+            this.Acc = acc;
+        }
+    }
+}
